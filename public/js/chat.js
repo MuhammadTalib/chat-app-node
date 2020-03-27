0164eb22fa68b82706778900da37997be53ae07a
MuhammadTalib <talibwaseem135@gmail.com>
2020-03-27 12:01:24 -0700

    first commit

---
+++ public/js/chat.js
@@ -0,0 +1,23 @@
+const socket = io()
+
+socket.on('message',(message)=>{
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const message = e.target.elements.message.value;
+    socket.emit('sendMessage',message)
+})
+
+document.querySelector('#send-location').addEventListener('click',()=>{
+    if(!navigator.geolocation){
+        return alert('Geo  Location is not supported by your Browser.')
+    }
+    navigator.geolocation.getCurrentPosition((position)=>{
+        socket.emit('sendLocation',{
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+})
